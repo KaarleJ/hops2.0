a5895eadf998eedb5fbf5e9b2a751d81a59e386e
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-17 14:36:38 +0300

    Added Providers

---
+++ src/components/ContextProvider/index.tsx
@@ -0,0 +1,21 @@
+import ThemeProvider from "./ThemeProvider";
+import AuthProvider from "./AuthProvider";
+
+export default function ContextProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <AuthProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </AuthProvider>
+  );
+}
