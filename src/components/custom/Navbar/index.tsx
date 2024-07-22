d642309bb01a8dc455035730d191079f1fc93ae8
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-22 13:23:15 +0300

    Fixed layout

--- src/components/custom/Navbar/index.tsx
+++ src/components/custom/Navbar/index.tsx
@@ -7,8 +7,8 @@ import { getServerSession } from "next-auth";
 export default async function Navbar() {
   const session = await getServerSession();
   return (
-    <div className="px-2 py-4 md:px-24 md:py-6 w-full fixed top-0 left-0 z-50">
-      <nav className="py-2 px-8 md:px-36 bg-primary shadow-xl rounded-full text-lg text-primary-foreground flex flex-row justify-between items-center">
+    <div className="px-2 py-4 md:px-24 md:py-6 w-screen fixed top-0 left-0">
+      <nav className="py-2 px-8 md:px-36 bg-primary shadow-xl rounded-full text-lg text-primary-foreground flex flex-row justify-between items-center md:mr-4">
         <Link href="/" className="hover:text-muted-foreground w-24">
           <b>Hops</b>
         </Link>
@@ -18,7 +18,7 @@ export default async function Navbar() {
         >
           calendar
         </Link>
-        <div className="hidden md:flex flex-row items-center justify-between">
+        <div className="hidden md:flex flex-row items-center">
           <ThemeToggle />
           {session ? (
             <AvatarMenu src={session.user?.image as string | undefined} />
