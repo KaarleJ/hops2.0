1a522c25fca8c0b11431269013266f653403b7df
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-22 13:09:10 +0300

    fixed width

--- src/app/calendar/layout.tsx
+++ src/app/calendar/layout.tsx
@@ -7,7 +7,7 @@ export default function CalendarPageLayout({
 }) {
   return (
     <div className="mt-28 flex min-h-screen flex-col items-center justify-start">
-      <div className="px-4 md:px-28 py-6 md:py-24 flex flex-col justify-start items-start w-screen">
+      <div className="px-4 md:px-28 py-6 md:py-24 flex flex-col justify-start items-start w-full">
         <h1>CalendarPage</h1>
         <Dashboard>{children}</Dashboard>
       </div>
