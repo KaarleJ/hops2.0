1534917dd70d28581f5c942d9860ad49035f8957
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-23 15:27:02 +0300

    Usin gyear from the url

--- src/app/(hops)/calendar/courselist/page.tsx
+++ src/app/(hops)/calendar/courselist/page.tsx
@@ -1,3 +1,8 @@
-export default function CourseList() {
-  return <div>CourseList</div>;
+import { getCourses } from "@/db/db";
+
+export default async function CourseList({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }) {
+  const year = Number(searchParams.year) || new Date().getFullYear();
+  const courses = await getCourses(year);
+
+  return <div>Courselist</div>;
 }
