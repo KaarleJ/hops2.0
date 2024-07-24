b4a7322b9dab5d7b99058107af098b562b54e305
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-24 14:48:53 +0300

    Fixed border

--- src/app/(hops)/calendar/page.tsx
+++ src/app/(hops)/calendar/page.tsx
@@ -9,9 +9,5 @@ export default async function CalendarPage({
   const year = Number(searchParams.year) || new Date().getFullYear();
   const courses = await getCourses(year);
 
-  return (
-    <div className="p-2 border-l border-accent">
-      <Calendar courses={courses} />
-    </div>
-  );
+  return <Calendar courses={courses} />;
 }
