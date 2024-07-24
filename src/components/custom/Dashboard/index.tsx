8319442851f776e7a54874307457cdd8d17c5695
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-24 14:34:00 +0300

    FIxed borders

--- src/components/custom/Dashboard/index.tsx
+++ src/components/custom/Dashboard/index.tsx
@@ -7,14 +7,14 @@ import { useState } from "react";
 
 export default function Dashboard({ children }: { children: React.ReactNode }) {
   const [collapsed, setCollapsed] = useState(false); // controls the side menu collapse
-  
+
   return (
     <div className="w-full h-full my-6 border border-border rounded-sm flex flex-row justify-start items-stretch relative">
-      <SideMenu collapsed={collapsed}/>
+      <SideMenu collapsed={collapsed} />
       <div className="w-full h-full rounded-sm flex flex-col justify-start">
-        <TopMenu collapsed={collapsed} setCollapsed={setCollapsed}/>
+        <TopMenu collapsed={collapsed} setCollapsed={setCollapsed} />
         <Separator />
-        {children}
+        <div className="p-2 border-l border-accent h-5/6">{children}</div>
       </div>
     </div>
   );
