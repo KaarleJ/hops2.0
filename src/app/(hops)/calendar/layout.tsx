ea0e569f78f8a7ac5c5223e5624ed0172ba711ff
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-24 15:07:49 +0300

    Added loading and changed layout to clientside

--- src/app/(hops)/calendar/layout.tsx
+++ src/app/(hops)/calendar/layout.tsx
@@ -1,15 +1,25 @@
+"use client";
 import Dashboard from "@/components/custom/Dashboard";
+import Loading from "@/components/custom/Loading";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 export default function CalendarPageLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const search = useSearchParams();
+  const key = JSON.stringify(Object.fromEntries(search.entries()));
   return (
     <div className="mt-28 flex min-h-screen flex-col items-center justify-start">
       <div className="px-4 md:px-28 py-6 md:py-24 flex flex-col justify-start items-start w-full">
         <h1>CalendarPage</h1>
-        <Dashboard>{children}</Dashboard>
+        <Dashboard>
+          <Suspense key={key} fallback={<Loading />}>
+            {children}
+          </Suspense>
+        </Dashboard>
       </div>
     </div>
   );
