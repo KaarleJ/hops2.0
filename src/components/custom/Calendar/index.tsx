7ba08e623f2be534d99c52970d0fd85d7544aab1
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-24 12:02:07 +0300

    Finished the basic grid layout

--- src/components/custom/Calendar/index.tsx
+++ src/components/custom/Calendar/index.tsx
@@ -1,13 +1,23 @@
 import { Course } from "@/types/types";
-import CourseButton from "./CourseButton";
+import { Button } from "@/components/ui/button";
 
-
-export default function Calendar({ courses }: { courses: Course[]}) {
+export default function Calendar({ courses }: { courses: Course[] }) {
   return (
-    <div className="grid grid-cols-4 grid-rows-6 auto-rows-max">
-      {courses.map((course) => (
-        <CourseButton course={course} key={course.id} />
-      ))}
+    <div className="grid grid-cols-4 grid-rows-5 grid-flow-row-dense gap-4 p-2">
+      {courses.map((course) => {
+        const span =
+          course.endPeriod === course.startPeriod
+            ? ""
+            : `col-span-${course.endPeriod - course.startPeriod + 1}`;
+        return (
+          <Button
+            className={`col-start-${course.startPeriod} ${span}`}
+            key={course.id}
+          >
+            {course.name} {course.startPeriod}-{course.endPeriod}
+          </Button>
+        );
+      })}
     </div>
   );
 }
