285f5c71a2cc9a5e9335440e751a21675aa1a493
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-24 12:15:07 +0300

    Wrapped auth/error in suspense

--- src/app/(auth)/auth/layout.tsx
+++ src/app/(auth)/auth/layout.tsx
@@ -1,5 +1,6 @@
 import Banner from "@/components/custom/Banner";
 import Footer from "@/components/custom/Footer";
+import { Suspense } from "react";
 
 export default function AuthPageLayout({
   children,
@@ -10,7 +11,9 @@ export default function AuthPageLayout({
     <div className="flex min-h-screen flex-col items-center w-full justify-start bg-[url('/landing.webp')] bg-center bg-cover">
       <div className="p-2 md:p-4 flex flex-col justify-center items-center w-full">
         <Banner />
-        <div className="w-full my-48 max-w-md">{children}</div>
+        <div className="w-full my-48 max-w-md">
+          <Suspense>{children}</Suspense>
+        </div>
       </div>
       <Footer />
     </div>
