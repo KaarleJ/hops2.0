1d9e039ad38e151af1257805f2cfa57b880be192
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-25 12:16:35 +0300

    Changed redirect url

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(req: NextRequest) {
   console.log("middleware");
   if (req.nextUrl.pathname === "/auth/signin") {
     if (req.cookies.get("next-auth.session-token")) {
-      return NextResponse.redirect(new URL("/", req.url));
+      return NextResponse.redirect(new URL("/calendar", req.url));
     }
   }
   return NextResponse.next();
