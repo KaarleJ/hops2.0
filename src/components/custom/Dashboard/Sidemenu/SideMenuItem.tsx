ec0ac21a9dc52b60a00cb5f833f83982b19be39f
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-25 12:54:12 +0300

    Fixed heights

--- src/components/custom/Dashboard/Sidemenu/SideMenuItem.tsx
+++ src/components/custom/Dashboard/Sidemenu/SideMenuItem.tsx
@@ -7,10 +7,10 @@ interface SideMenuItemProps {
 
 export default function SideMenuItem({ children, to }: SideMenuItemProps) {
   return (
-    <div className="p-1 w-full h-full text-sm">
+    <div className="p-1 w-full h-min text-sm my-2 py-1">
       <Link
         href={"/calendar" + (to ? `/${to}` : "")}
-        className="p-1 w-full h-full hover:bg-accent rounded-sm transition-colors text-left flex flex-row"
+        className="p-1 w-full hover:bg-accent rounded-sm transition-colors text-left flex flex-row"
       >
         {children}
       </Link>
--- src/components/custom/Dashboard/index.tsx
+++ src/components/custom/Dashboard/index.tsx
@@ -9,12 +9,12 @@ export default function Dashboard({ children }: { children: React.ReactNode }) {
   const [collapsed, setCollapsed] = useState(false); // controls the side menu collapse
 
   return (
-    <div className="w-full h-full my-6 border border-border rounded-sm flex flex-row justify-start items-stretch">
+    <div className="w-full h-[32rem] my-6 border border-border rounded-sm flex flex-row justify-start items-stretch relative">
       <SideMenu collapsed={collapsed} />
       <div className="w-full h-full rounded-sm flex flex-col justify-start">
         <TopMenu collapsed={collapsed} setCollapsed={setCollapsed} />
         <Separator />
-        <div className="p-2 border-l border-accent h-5/6">{children}</div>
+        <div className="p-2 border-l border-accent h-full">{children}</div>
       </div>
     </div>
   );
