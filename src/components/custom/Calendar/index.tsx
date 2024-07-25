cf6756692557b10f76b182b7e4e3fdcaf1ae00c7
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-25 15:35:04 +0300

    ...

--- src/components/custom/Calendar/index.tsx
+++ src/components/custom/Calendar/index.tsx
@@ -2,6 +2,10 @@ import { Course } from "@/types/types";
 import { Button } from "@/components/ui/button";
 
 export default function Calendar({ courses }: { courses: Course[] }) {
+  if (courses.length === 0) {
+    return <p className="p-12">No courses added yet</p>;
+  }
+
   return (
     <div className="grid grid-cols-4 grid-rows-5 grid-flow-row-dense gap-4 p-2">
       {courses.map((course) => {
