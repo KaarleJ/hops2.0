b041fa01bb537e5c68a81bbdc8a816e30f940e89
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-25 12:54:33 +0300

    Fixed

--- src/components/custom/Loading.tsx
+++ src/components/custom/Loading.tsx
@@ -3,7 +3,7 @@ import { LoaderCircle } from "lucide-react";
 export default function Loading({ className }: { className?: string }) {
   return (
     <div className={`w-full h-full flex items-stretch justify-center ${className}`}>
-      <LoaderCircle className="animate-spin text-6xl" />
+      <LoaderCircle className="animate-spin" size={42}/>
     </div>
   );
 }
