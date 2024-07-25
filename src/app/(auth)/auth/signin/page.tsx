312eee8e9b46e50b3b7fa804e84222faba29906a
Kaarle Järvinen <kaarle46@gmail.com>
2024-07-25 12:09:27 +0300

    Singin page is now static and redirect logic moved to middleware

--- src/app/(auth)/auth/signin/page.tsx
+++ src/app/(auth)/auth/signin/page.tsx
@@ -1,12 +1,11 @@
 import { getProviders } from "next-auth/react";
 import SignInCard from "@/components/custom/auth/SignInCard";
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
+
+export const dynamic = "force-static";
+export const revalidate = 300;
+
 export default async function SignIn() {
   const providers = await getProviders();
-  const session = await getServerSession();
-
-  session && redirect("/");
 
   return <SignInCard providers={providers} />;
 }
---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export async function middleware(req: NextRequest) {
+  console.log("middleware");
+  if (req.nextUrl.pathname === "/auth/signin") {
+    if (req.cookies.get("next-auth.session-token")) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/auth/signin",
+};
