62b80d97b202c6c549032343cab6cdb1ae627b6a
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-01 11:57:44 +0300

    Added footer to calendar

--- src/components/custom/Calendar/index.tsx
+++ src/components/custom/Calendar/index.tsx
@@ -8,22 +8,36 @@ export default function Calendar({ courses }: { courses: Course[] }) {
   }
 
   return (
-    <div className="grid grid-cols-4 grid-flow-row-dense gap-4 p-4">
-      {courses.map((course) => {
-        // We apply grid attributes by using style prop since tailwind has bugs with grid
-        return (
-          <CourseModal course={course} key={course.name}>
-            <Button
-              style={{
-                gridColumnStart: course.startPeriod,
-                gridColumnEnd: course.endPeriod + 1,
-              }}
-            >
-              <span className="mx-6">{course.name}</span> {course.ects} ECTS
-            </Button>
-          </CourseModal>
-        );
-      })}
+    <div className="flex flex-col justify-between h-full">
+      <div className="grid grid-cols-4 grid-flow-row-dense gap-4 p-4">
+        {courses.map((course) => {
+          // We apply grid attributes by using style prop since tailwind has bugs with grid
+          return (
+            <CourseModal course={course} key={course.name}>
+              <Button
+                style={{
+                  gridColumnStart: course.startPeriod,
+                  gridColumnEnd: course.endPeriod + 1,
+                }}
+              >
+                <span className="mx-6">{course.name}</span> {course.ects} ECTS
+              </Button>
+            </CourseModal>
+          );
+        })}
+      </div>
+      <CalendarFooter />
+    </div>
+  );
+}
+
+function CalendarFooter() {
+  return (
+    <div className="grid grid-cols-4 text-center border-t border-accent px-4">
+      <p className="py-2">1st period</p>
+      <p className="border-x border-accent py-2">2nd period</p>
+      <p className="border-x border-accent py-2">3rd period</p>
+      <p className="py-2">4th period</p>
     </div>
   );
 }
