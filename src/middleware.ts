38958c021e047687e66d1672dbffff10ce4d6091
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-02 11:04:23 +0300

    ...

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
-  console.log("middleware");
   if (req.nextUrl.pathname === "/auth/signin") {
     if (req.cookies.get("next-auth.session-token")) {
       return NextResponse.redirect(new URL("/calendar", req.url));
