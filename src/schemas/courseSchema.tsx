c9252149bd422d47d65ceae616f447cc51e84825
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-02 12:56:21 +0300

    Cahnged values to strings

--- src/schemas/courseSchema.tsx
+++ src/schemas/courseSchema.tsx
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const courseSchema = z.object({
   name: z.string(),
   code: z.string(),
-  ects: z.number(),
-  year: z.number(),
-  startPeriod: z.number().min(1).max(5),
-  endPeriod: z.number().min(1).max(5),
+  ects: z.string().regex(/^[0-5]$/, 'Period must be between 0 and 5'),
+  year: z.string().regex(/^[0-9]{4}$/, 'Year must be a number'),
+  startPeriod: z.string().regex(/^[0-5]$/, 'Period must be between 0 and 5'),
+  endPeriod: z.string().regex(/^[0-5]$/, 'Period must be between 0 and 5'),
 })
