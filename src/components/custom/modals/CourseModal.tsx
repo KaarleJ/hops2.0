45947e0a32a8deb8310c7ba0dd28ac790ee597cf
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-02 12:56:55 +0300

    Refactored and added layout for delete and edit actions

---
+++ src/components/custom/modals/CourseModal.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "../../ui/button";
+import {
+  Modal,
+  ModalClose,
+  ModalContent,
+  ModalDescription,
+  ModalHeader,
+  ModalTitle,
+  ModalTrigger,
+  ModalBody,
+} from "../../ui/modal";
+import { Course } from "@/types/types";
+import { Form } from "@/components/ui/form";
+import CourseFormFields from "../CourseFormFields";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { courseSchema } from "@/schemas/courseSchema";
+import { z } from "zod";
+
+interface CourseModalProps {
+  course: Course;
+  children?: React.ReactNode;
+}
+
+export default function CourseModal({ course, children }: CourseModalProps) {
+  const [edit, setEdit] = useState(false);
+
+  const form = useForm<z.infer<typeof courseSchema>>({
+    resolver: zodResolver(courseSchema),
+    defaultValues: {
+      name: course.name,
+      code: course.code,
+      ects: course.ects.toString(),
+      year: course.year.toString(),
+      startPeriod: course.startPeriod.toString(),
+      endPeriod: course.endPeriod.toString(),
+    },
+  });
+
+  return (
+    <Modal onOpenChange={() => setEdit(false)}>
+      <ModalTrigger asChild className="hover:cursor-pointer">
+        {children}
+      </ModalTrigger>
+      <ModalContent>
+        <ModalHeader>
+          <ModalTitle>{course.name}</ModalTitle>
+          <ModalDescription>{course.code}</ModalDescription>
+        </ModalHeader>
+        <ModalBody>
+          {edit ? (
+            <Form {...form}>
+              <form action={() => console.log("submit")} className="space-y-8">
+                <CourseFormFields form={form} />
+                <div className="flex flex-row justify-between">
+                  <Button type="submit">Submit</Button>
+                  <ModalClose>Cancel</ModalClose>
+                </div>
+              </form>
+            </Form>
+          ) : (
+            <>
+              <div className="py-4">
+                <p>Year: {course.year}</p>
+                <p>Start period: {course.startPeriod}</p>
+                <p>End period: {course.endPeriod}</p>
+              </div>
+              <div className="py-4">
+                <p>ECTS: {course.ects}</p>
+                <p>
+                  Length: {course.endPeriod - course.startPeriod + 1} periods
+                </p>
+              </div>
+            </>
+          )}
+        </ModalBody>
+        {!edit && (
+          <div className="flex justify-between">
+            <div className="flex">
+              <Button onClick={() => setEdit(true)}>Edit</Button>
+              <form className="ml-6" action={() => console.log("delete")}>
+                <Button type="submit" variant="destructive">
+                  Delete
+                </Button>
+              </form>
+            </div>
+            <ModalClose>Close</ModalClose>
+          </div>
+        )}
+      </ModalContent>
+    </Modal>
+  );
+}
