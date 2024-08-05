33b65abfcbeebe5cb49d35665c743d15f2bb4206
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-05 11:11:43 +0300

    Moved calendarfooter to layout

--- src/app/(hops)/calendar/layout.tsx
+++ src/app/(hops)/calendar/layout.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Dashboard from "@/components/custom/Dashboard";
-import { useSearchParams } from "next/navigation";
+import { usePathname, useSearchParams } from "next/navigation";
 import { Suspense } from "react";
 
 export default function CalendarPageLayout({
@@ -9,17 +9,35 @@ export default function CalendarPageLayout({
   children: React.ReactNode;
 }) {
   const search = useSearchParams();
+  const path = usePathname();
+
+  const isCalendar = path === "/calendar";
   const key = JSON.stringify(Object.fromEntries(search.entries()));
+
   return (
     <div className="mt-28 flex min-h-screen flex-col items-center justify-start">
       <div className="px-4 md:px-28 py-6 md:py-24 flex flex-col justify-start items-start w-full">
         <h1>CalendarPage</h1>
         <Dashboard>
           <Suspense key={key}>
-            {children}
+            <div className={`${isCalendar && 'flex flex-col-reverse justify-between'} h-full`}>
+              {isCalendar && <CalendarFooter />}
+              {children}
+            </div>
           </Suspense>
         </Dashboard>
       </div>
+    </div>
+  );
+}
+
+function CalendarFooter() {
+  return (
+    <div className="grid grid-cols-4 text-center border-t border-accent px-4">
+      <p className="py-2">1st period</p>
+      <p className="border-x border-accent py-2">2nd period</p>
+      <p className="border-x border-accent py-2">3rd period</p>
+      <p className="py-2">4th period</p>
     </div>
   );
 }
