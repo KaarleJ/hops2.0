0e0ad12573c77a95e14417e3d3fe0521c9aad924
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-06 14:03:17 +0300

    Added callbacks to add user to database and add id to user object

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,6 @@
+import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { PrismaClient } from "@prisma/client";
 
 const github_id = process.env.GITHUB_ID;
 const github_secret = process.env.GITHUB_SECRET;
@@ -7,7 +9,55 @@ if (!github_id || !github_secret) {
   throw new Error("GITHUB_ID and GITHUB_SECRET must be set");
 }
 
-export const authOptions = {
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        const dbUser = await prisma.user.findUnique({
+          where: {
+            email: user.email as string | undefined,
+          },
+        });
+        await prisma.$disconnect();
+        if (!dbUser) {
+          throw new Error("No user found");
+        }
+        token.id = dbUser.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id;
+      }
+      return session;
+    },
+    async signIn({ user }) {
+      let prismaUser = await prisma.user.findUnique({
+        where: { email: user.email as string | undefined },
+      });
+
+      try {
+        if (!prismaUser) {
+          const dbUser = {
+            name: user.name as string,
+            email: user.email as string,
+            avatarUrl: user.image as string,
+          };
+          prismaUser = await prisma.user.create({ data: dbUser });
+        }
+        user.id = prismaUser.id;
+        return true;
+      } catch (error) {
+        console.error("Error signing in:", error);
+        return false;
+      } finally {
+        await prisma.$disconnect();
+      }
+    },
+  },
   providers: [
     GithubProvider({
       clientId: github_id,
