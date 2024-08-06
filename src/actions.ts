47fed32b5a31c74134f7869418f41aee7781a2e5
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-06 14:03:59 +0300

    Created prisma schema and made actions use prisma for db queries

--- src/actions.ts
+++ src/actions.ts
@@ -1,10 +1,22 @@
 "use server";
-import { Course as CourseType, FormCourse } from "../types";
+import { getServerSession } from "next-auth";
+import { FormCourse } from "../types";
+import { PrismaClient } from "@prisma/client";
+import { authOptions } from "../auth.config";
+import { revalidatePath } from 'next/cache'
 
-const delay = () => new Promise((resolve) => setTimeout(resolve, 200));
+const prisma = new PrismaClient();
 
 export async function getCourses(year: number) {
-  return courses.filter((course) => course.year === year);
+  const session = await getServerSession(authOptions);
+  try {
+    const courses = await prisma.course.findMany({
+      where: { year, authorId: session?.user?.id },
+    });
+    return courses;
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function addCourse({
@@ -15,135 +27,74 @@ export async function addCourse({
   startPeriod,
   endPeriod,
 }: FormCourse) {
-  console.log("Adding course:", name, code, ects, year, startPeriod, endPeriod);
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    throw new Error("Unauthorized");
+  }
+
+  try {
+    return await prisma.course.create({
+      data: {
+        name,
+        code,
+        ects: parseInt(ects),
+        year: parseInt(year),
+        startPeriod: parseInt(startPeriod),
+        endPeriod: parseInt(endPeriod),
+        authorId: session.user.id,
+      },
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
-export async function updateCourse(course: CourseType) {
-  await delay();
-  console.log("Course updated:", course);
+export async function updateCourse(
+  { name, code, ects, year, startPeriod, endPeriod }: FormCourse,
+  id: string
+) {
+  const session = await getServerSession(authOptions);
+  const dbCourse = await prisma.course.findUnique({
+    where: { id },
+  });
+
+  if (!dbCourse || dbCourse.authorId !== session?.user.id) {
+    throw new Error("Unauthorized");
+  }
+
+  try {
+    return await prisma.course.update({
+      where: { id },
+      data: {
+        name,
+        code,
+        ects: parseInt(ects),
+        year: parseInt(year),
+        startPeriod: parseInt(startPeriod),
+        endPeriod: parseInt(endPeriod),
+      },
+    });
+  } finally {
+    await prisma.$disconnect();
+  }
 }
 
 export async function deleteCourse(id: string) {
-  await delay();
-  console.log("Course deleted:", id);
-}
+  const session = await getServerSession(authOptions);
+  const dbCourse = await prisma.course.findUnique({
+    where: { id },
+  });
 
-const courses: CourseType[] = [
-  {
-    id: "1",
-    name: "Math",
-    year: 2024,
-    code: "MATH101",
-    ects: 5,
-    startPeriod: 1,
-    endPeriod: 1,
-  },
-  {
-    id: "2",
-    name: "Physics",
-    year: 2024,
-    code: "PHYS101",
-    ects: 5,
-    startPeriod: 3,
-    endPeriod: 3,
-  },
-  {
-    id: "3",
-    name: "Chemistry",
-    year: 2024,
-    code: "CHEM101",
-    ects: 5,
-    startPeriod: 4,
-    endPeriod: 4,
-  },
-  {
-    id: "4",
-    name: "Biology",
-    year: 2024,
-    code: "BIO101",
-    ects: 5,
-    startPeriod: 2,
-    endPeriod: 2,
-  },
-  {
-    id: "5",
-    name: "Computer Science",
-    year: 2024,
-    code: "CS101",
-    ects: 5,
-    startPeriod: 3,
-    endPeriod: 4,
-  },
-  {
-    id: "6",
-    name: "History",
-    year: 2024,
-    code: "HIST101",
-    ects: 5,
-    startPeriod: 1,
-    endPeriod: 1,
-  },
-  {
-    id: "7",
-    name: "Geography",
-    year: 2025,
-    code: "GEOG101",
-    ects: 5,
-    startPeriod: 4,
-    endPeriod: 4,
-  },
-  {
-    id: "8",
-    name: "Economics",
-    year: 2025,
-    code: "ECON101",
-    ects: 5,
-    startPeriod: 2,
-    endPeriod: 2,
-  },
-  {
-    id: "9",
-    name: "Literature",
-    year: 2025,
-    code: "LIT101",
-    ects: 5,
-    startPeriod: 3,
-    endPeriod: 3,
-  },
-  {
-    id: "10",
-    name: "Philosophy",
-    year: 2025,
-    code: "PHIL101",
-    ects: 5,
-    startPeriod: 1,
-    endPeriod: 1,
-  },
-  {
-    id: "11",
-    name: "Psychology",
-    year: 2021,
-    code: "PSYCH101",
-    ects: 5,
-    startPeriod: 4,
-    endPeriod: 4,
-  },
-  {
-    id: "12",
-    name: "Sociology",
-    year: 2023,
-    code: "SOC101",
-    ects: 5,
-    startPeriod: 2,
-    endPeriod: 2,
-  },
-  {
-    id: "13",
-    name: "Art",
-    year: 2023,
-    code: "ART101",
-    ects: 5,
-    startPeriod: 3,
-    endPeriod: 3,
-  },
-];
+  if (!dbCourse || dbCourse.authorId !== session?.user.id) {
+    throw new Error("Unauthorized");
+  }
+
+  try {
+    await prisma.course.delete({
+      where: { id },
+    });
+    revalidatePath('/calendar')
+  } finally {
+    await prisma.$disconnect();
+  }
+}
