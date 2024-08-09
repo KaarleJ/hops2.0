e9359b8f79d15e86b049d21cc705c55c70e32e22
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-09 13:17:02 +0300

    fix for vercel

--- src/app/(auth)/auth/signin/page.tsx
+++ src/app/(auth)/auth/signin/page.tsx
@@ -1,11 +1,19 @@
 import { getProviders } from "next-auth/react";
 import SignInCard from "@/components/custom/auth/SignInCard";
+import { authOptions } from "../../../../../auth.config";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
 export const dynamic = "force-static";
 export const revalidate = 300;
 
 export default async function SignIn() {
+  const session = await getServerSession(authOptions);
   const providers = await getProviders();
+
+  if (session) {
+    redirect("/");
+  }
 
   return <SignInCard providers={providers} />;
 }
