bf34d6a5396c1f1ac4ee9235af700a55fa9e02a4
Kaarle Järvinen <kaarle46@gmail.com>
2024-08-14 14:23:11 +0300

    Fixed schema

--- src/schemas/courseSchema.tsx
+++ src/schemas/courseSchema.tsx
@@ -6,7 +6,8 @@ export const courseFormSchema = z.object({
     .min(3, "Name must be at least 3 characters")
     .max(255, "Name must be at most 255 characters"),
   code: z.string(),
-  ects: z.string().regex(/^[0-5]$/, "Period must be between 0 and 5"),
+  // ECTS is a number, but we want to display it as a string in the form
+  ects: z.string().regex(/^[0-9]+$/, "ECTS must be a number"),
   year: z.string().regex(/^[0-9]{4}$/, "Year must be a number"),
   startPeriod: z.string().regex(/^[0-5]$/, "Period must be between 0 and 5"),
   endPeriod: z.string().regex(/^[0-5]$/, "Period must be between 0 and 5"),
